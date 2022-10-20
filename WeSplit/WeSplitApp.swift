67bd0df03792b548f68472f87190880e52d28795
Daniel Grec <104867807+DanielGrec@users.noreply.github.com>
2022-10-20 18:56:11 +0300

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Grec Daniel on 20.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
